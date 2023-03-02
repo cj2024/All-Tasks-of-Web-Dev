749a7b0fcf297b7a842dea14b08e3ca10a093641
cj2024 <126644942+cj2024@users.noreply.github.com>
2023-03-02 15:56:05 +0530

    Delete Task1 directory

--- Task1/dom.js
+++
@@ -1,6 +0,0 @@
-var title = document.getElementById('main-header');
-title.style.borderBottom = 'solid 3px #000';
-
-var addItems = document.getElementById('add-items');
-addItems.style.fontWeight = 'bold';
-addItems.style.color = 'green';
