afb319a2a31c2ee9ee676be1c9e82a8784c78254
cj2024 <chinmayj2610@gmail.com>
2023-03-02 12:42:37 +0530

    Task 3

---
+++ Task3/index.js
@@ -0,0 +1,6 @@
+var newLi = document.getElementsByClassName('new-list-item');
+newLi[0].style.color = 'green';
+
+var li = document.getElementsByTagName('li');
+li[4].style.backgroundColor = 'grey';
+li[4].style.fontWeight = 'bold';
