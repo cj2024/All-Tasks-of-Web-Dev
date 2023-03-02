df21b9a6bd3cf74b13a830dd7aed4075a4416150
cj2024 <chinmayj2610@gmail.com>
2023-03-02 11:34:43 +0530

    Task 1

---
+++ Task1/dom.js
@@ -0,0 +1,6 @@
+var title = document.getElementById('main-header');
+title.style.borderBottom = 'solid 3px #000';
+
+var addItems = document.getElementById('add-items');
+addItems.style.fontWeight = 'bold';
+addItems.style.color = 'green';
